644c2389c2092cc700874bd6a79f1ffe071803fa
MATEUS FELIPE DIAS <0058564@ifmg.edu.br>
2023-06-28 09:09:22 -0300

    origin master

---
+++ Atleta/Triatleta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atleta
+{
+    public class Triatleta:Atleta
+    {
+
+        public override void Habilidades()
+        {
+            //base.Habilidades();
+            MessageBox.Show("Correr,pedalar e nadar");
+        }
+
+        public override void acao()
+        {
+            MessageBox.Show("O atleta está correndo, pedalando e nadando.");
+
+        }
+
+    }
+}
